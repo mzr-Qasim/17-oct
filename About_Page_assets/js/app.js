77de8940ba2f30c7eff3c937162b6bdb7cd3232e
Muhammad Qasim <malikqasim20051@gmail.com>
2023-10-17 12:31:51 -0700

    Home Page

---
+++ About_Page_assets/js/app.js
@@ -0,0 +1,90 @@
+
+
+const choiceArray = document.querySelectorAll(".choice")
+
+
+
+let playButton = document.getElementById("play_button");
+let pauseButton = document.getElementById("pause_button");
+
+
+
+
+jQuery(document).ready(function($) {
+
+
+    $(document).on('click', '.owl-item>div', function() {
+      // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+      var $speed = 300;  // in ms
+      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000
+    });
+
+    var $owl = $('.owl-carousel');
+    
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+    
+    $owl.owlCarousel({
+      responsive:{
+        0:{
+            items:1,
+            center:true,
+            nav:true,
+            loop:true,
+            dots:false,
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop:true,
+            dots:true,
+            center:true,
+            autoplay:true
+        },
+        831:{
+            items:3,
+            nav:true,
+            loop:true,
+            dots:true,
+            center:true,
+            autoplay:true
+        },
+        1464:{
+            center:true,
+            loop:true,
+            items:5,
+            nav:false,
+            autoplay:true,
+            dots:true,
+        },
+        1800:{
+            center:true,
+            loop:true,
+            items:5,
+            nav:false,
+            autoplay:true,
+            dots:true,
+        },
+
+    }
+      
+    });
+
+    
+
+    
+
+
+
+
+
+    
+});
+
+
---
+++ assets/js/app.js
@@ -0,0 +1,181 @@
+
+
+const choiceArray = document.querySelectorAll(".choice")
+
+
+
+let playButton = document.getElementById("play_button");
+let pauseButton = document.getElementById("pause_button");
+
+
+
+
+jQuery(document).ready(function($) {
+
+    choiceArray.forEach((card) => {
+        card.addEventListener("click", () => {
+            choiceArray.forEach((element) => {
+                element.classList.remove("expand", "unset")
+                element.classList.add('small')
+            })
+            card.classList.remove("small")
+            card.classList.add('expand')
+        });
+    });
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var $owl = $('.owl-carousel5');
+    
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+    $owl.owlCarousel({
+      responsive:{
+        0:{
+            items:1,
+            center:true,
+            nav:true,
+            loop:true,
+            dots:false,
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop:false,
+            dots:true,
+        },
+        1000:{
+            center:true,
+            loop:true,
+            items:3,
+            nav:false,
+            autoplay:true,
+        },
+
+    }
+      
+    });
+    
+    $(document).on('click', '.owl-item>div', function() {
+      // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+      var $speed = 300;  // in ms
+      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000
+    });
+
+    playButton.addEventListener("click", function() {
+        video.play();
+        playButton.classList.add('vplay');
+        pauseButton.classList.remove('vpause');
+        document.querySelector('html').classList.add('playvideo');
+      });
+      
+      pauseButton.addEventListener("click", function() {
+          video.pause();
+           playButton.classList.remove('vplay');
+        pauseButton.classList.add('vpause');
+      });
+      //Video Play Pause Control End
+      //Video Sound on & off Ctrl Start
+      let soundOff = document.getElementById("sound-off");
+      let soundOn = document.getElementById("sound-on");
+      let soundCtrl = document.querySelector(".sound-ctrl");
+      
+      soundOff.addEventListener("click", function() {
+       video.muted = true;
+       soundCtrl.classList.add('soundctrlshow');
+      });
+      
+      soundOn.addEventListener("click", function() {
+          video.muted = false;
+          soundCtrl.classList.remove('soundctrlshow'); 
+      });
+      //Video Sound on & off Ctrl End
+      video.onended = function() {
+              document.querySelector('html').classList.remove('playvideo');
+           playButton.classList.remove('vplay');
+      };
+
+
+       
+    var $owl = $('.owl-carousel6');
+    
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+    $owl.owlCarousel({
+      responsive:{
+        0:{
+            items:1,
+            center:true,
+            nav:true,
+            loop:true,
+            dots:false,
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop:true,
+            dots:true,
+            center:true,
+            autoplay:true
+        },
+        831:{
+            items:3,
+            nav:true,
+            loop:true,
+            dots:false,
+            center:true,
+            autoplay:true
+        },
+        1464:{
+            center:true,
+            loop:true,
+            items:5,
+            nav:false,
+            autoplay:true,
+            dots:false
+        },
+        1800:{
+            center:true,
+            loop:true,
+            items:5,
+            nav:false,
+            autoplay:true,
+            dots:false,
+
+        },
+
+    }
+      
+    });
+
+
+   
+    
+      
+
+
+    
+
+    
+
+
+
+    
+});
+
