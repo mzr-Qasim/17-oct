4334038673537446f4778ba63f73c4b9ec26bc53
Muhammad Qasim <malikqasim20051@gmail.com>
2023-10-17 12:46:35 -0700

    Home Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,16 +66,6 @@ jQuery(document).ready(function($) {
       
     });
     
-    $(document).on('click', '.owl-item>div', function() {
-      // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-      var $speed = 300;  // in ms
-      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
-    });
-
-    $('.counter').counterUp({
-        delay: 10,
-        time: 5000
-    });
 
     playButton.addEventListener("click", function() {
         video.play();
