6cdb52e093ed93f30885fe73f0a1d4721bad7809
Muhammad Qasim <malikqasim20051@gmail.com>
2023-10-22 12:43:58 -0700

    About Page

--- About_Page_assets/js/app.js
+++ About_Page_assets/js/app.js
@@ -100,6 +100,98 @@ jQuery(document).ready(function($) {
         
     });
 
+
+    $('.owl-carousel-blog-section').owlCarousel({
+        nav:true,
+        //loop:true,
+        slideBy:'page',
+        rewind:false,
+        responsive:{
+            0:{
+                items:1,
+                autoplay:true,
+                loop:true,
+                center:true
+            },
+            480:{
+                items:1,
+                autoplay:true,
+                loop:true,
+                center:true
+            },
+            600:{
+                items:1,
+                autoplay:true,
+                loop:true,
+                center:true
+            },
+            767:{
+                items:2,
+                autoplay:true,
+                loop:true,
+            },
+            1000:{
+                items:2,
+                autoplay:true,
+                loop:true,
+            },
+            1199:{
+                items:3,
+                autoplay:true,
+                center:true,
+                loop:true
+            }
+        },
+        smartSpeed:70,
+        onInitialized:function(e){
+            $(e.target).find('img').each(function(){
+                if(this.complete){
+                    $(this).closest('.mhn-inner').find('.loader-circle').hide();
+                    $(this).closest('.mhn-inner').find('.mhn-img').css('background-image','url('+$(e.target).attr('src')+')');
+                }else{
+                    $(this).bind('load',function(e){
+                        $(e.target).closest('.mhn-inner').find('.loader-circle').hide();
+                        $(e.target).closest('.mhn-inner').find('.mhn-img').css('background-image','url('+$(e.target).attr('src')+')');
+                    });
+                }
+            });
+        },
+        navText:['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>','<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>']
+    });
+
+
+
+    var $owl = $('.owl-carousel5');
+    
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+    $owl.owlCarousel({
+      responsive:{
+        0:{
+            items:1,
+            center:true,
+            nav:true,
+            loop:true,
+            dots:false,
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop:false,
+            dots:true,
+        },
+        1000:{
+            center:true,
+            loop:true,
+            items:3,
+            nav:false,
+            autoplay:true,
+        },
+
+    }
+      
+    });
     
 
 
